d5266d64b38086e6bec327762622b0bb6043c3f6
LEGION <LEGION@172.25.43.219>
2024-04-02 08:22:11 +0700

    Sửa dòng 9 "Nhap a:" thành "Bien a:" Sửa dòng 11 "Nhap b:" thành "Bien b:"

--- src/GroupProject/PhepCong.java
+++ src/GroupProject/PhepCong.java
@@ -6,9 +6,9 @@ public class PhepCong extends GiaiToan{
     public int PhepCong() {
         int a, b;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Nhap a: ");
+        System.out.print("Bien a: ");
         a = sc.nextInt();
-        System.out.print("Nhap b: ");
+        System.out.print("Bien b: ");
         b = sc.nextInt();
         return a + b;
     }
